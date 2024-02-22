72dc9c65a273b4028fd46a73219e1a1062b707ed
natkam <natka_m@o2.pl>
2024-02-22 23:18:01 +0100

    Fix the variable names, add an explanatory comment for my future self

--- misc/safe.hs
+++ misc/safe.hs
@@ -8,6 +8,15 @@ safeSqrt x =
 safeRecSqrt x =
   safeSqrt x
     >>= \y ->
-      if x == 0
+      if y == 0
         then Left "div by 0"
-        else return (1 / x)
+        else return (1 / y)
+
+{-
+According to Bartosz ("Haskell-5-2"), the `Either` monad defines:
+```
+  return x = Right x
+```
+Therefore, we could write `else Right (1 / y)` instead of
+`else return (1 / y)`.
+-}
