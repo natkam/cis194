65bae10f3d4959a17e3a30765bf3e55d538b95d9
natkam <natka_m@o2.pl>
2024-03-01 03:26:39 +0100

    CIS194, HW1: Add documentation comments, rename a variable

--- cis194/homework1/CardNumbers.hs
+++ cis194/homework1/CardNumbers.hs
@@ -2,18 +2,22 @@ module CardNumbers where
 
 import Data.Char (digitToInt)
 
+-- Convert an integer into a list of its digits (as integers).
 toDigits :: Integer -> [Integer]
 toDigits n
   | n <= 0 = []
   | otherwise = map (toInteger . digitToInt) (show n)
 
+-- Convert an integer into a list of its digits (as integers) reversed.
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev = reverse . toDigits
 
+-- Double every other number in the input list, starting from the right,
+--   from the second-to-last element.
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther n =
+doubleEveryOther input =
   reverse
     ( map
-        (\(x, y) -> if even y then 2 * x else x)
-        (zip (reverse n) [1 ..])
+        (\(x, i) -> if even i then 2 * x else x)
+        (zip (reverse input) [1 ..])
     )
