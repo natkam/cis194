0a59baf46bdb4c74254b609b8fa792ea6c362136
natkam <natka_m@o2.pl>
2024-03-01 03:04:04 +0100

    CIS194, HW1: Rewrite toDigits using the guards notation

--- cis194/homework1/CardNumbers.hs
+++ cis194/homework1/CardNumbers.hs
@@ -3,10 +3,9 @@ module CardNumbers where
 import Data.Char (digitToInt)
 
 toDigits :: Integer -> [Integer]
-toDigits n = do
-  if n <= 0
-    then []
-    else map (toInteger . digitToInt) $ show n
+toDigits n
+  | n <= 0 = []
+  | otherwise = map (toInteger . digitToInt) $ show n
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev = reverse . toDigits
