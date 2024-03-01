3191ba91562e2da130e0cef076600826e98e6cf3
natkam <natka_m@o2.pl>
2024-03-01 03:13:36 +0100

    CIS194, HW1: Simplif doubleEveryOther, small style improvements

--- cis194/homework1/CardNumbers.hs
+++ cis194/homework1/CardNumbers.hs
@@ -5,10 +5,15 @@ import Data.Char (digitToInt)
 toDigits :: Integer -> [Integer]
 toDigits n
   | n <= 0 = []
-  | otherwise = map (toInteger . digitToInt) $ show n
+  | otherwise = map (toInteger . digitToInt) (show n)
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev = reverse . toDigits
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther n = reverse (map (\x -> if (even . snd) x then 2 * fst x else fst x) (zip (reverse n) [1 ..]))
+doubleEveryOther n =
+  reverse
+    ( map
+        (\(x, y) -> if even y then 2 * x else x)
+        (zip (reverse n) [1 ..])
+    )
