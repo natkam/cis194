b72e9bd7b9eae7061a6752eb2d6e9134ad66a543
natkam <natka_m@o2.pl>
2024-11-17 02:08:04 +0100

    CIS194, HW3: Code golf - ex. 3, histogram, 1st messy version

--- brent/homework03/GolfN.hs
+++ brent/homework03/GolfN.hs
@@ -1,5 +1,10 @@
 module Golf where
 
+import Data.List (group, nub, sort, transpose)
+
+--import Data.List.Unique (count)
+--import Data.Map (fromListWith, toList)
+
 skips :: [a] -> [[a]]
 skips xs = concatMap (getEveryNth xs) [1 .. (length xs)]
 
@@ -32,3 +37,31 @@ localMaxima xs =
     | i <- [1 .. length xs - 2],
       xs !! (i - 1) < xs !! i && xs !! (i + 1) < xs !! i
   ]
+
+histogram :: [Integer] -> String
+histogram xs = foldr1 (\c acc -> acc ++ "\n" ++ c) (getLines xs)
+
+--count' :: [Integer] -> [(Integer, Int)]
+--count' xs = zip ((sort . nub) xs) (map length ((group . sort) xs))
+
+--count'' :: [Integer] -> [(Integer, Int)]
+--count'' xs = toList (fromListWith (+) [(x, 1) | x <- xs])
+
+--count' xs = count xs
+
+getLines :: [Integer] -> [String]
+getLines xs = concatMap show [0 .. 9] : replicate 10 '=' : (transpose . getCols) xs
+
+-- Liczenie zerżnięte ze SO https://stackoverflow.com/q/19554984/4744341
+--cnt :: Eq a => a -> [a] -> Int
+--cnt x = length . filter (== x)
+
+count :: [Integer] -> [(Integer, Int)]
+count xs = [(x, (length . filter (== x)) xs) | x <- [0 .. 9]]
+
+-- A helper function to get the max number of occurrences of any number
+maxCount :: [Integer] -> Int
+maxCount xs = maximum (map length ((group . sort) xs))
+
+getCols :: [Integer] -> [String]
+getCols xs = [replicate (snd pair) char ++ replicate (maxCount xs - snd pair) ' ' | pair <- count xs, char <- if snd pair == 0 then " " else "*"]
