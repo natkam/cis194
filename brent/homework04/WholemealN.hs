559cf54fd922f943abb777f581834d565c9a30e6
natkam <3858688+natkam@users.noreply.github.com>
2024-12-08 04:16:31 +0100

    CIS194, HW04: [WIP] ex. 2 (wrong heights), ex.3.1, ex.3.2 + tests

--- brent/homework04/WholemealN.hs
+++ brent/homework04/WholemealN.hs
@@ -32,3 +32,50 @@ fun2' =
     )
 
 -- TODO: Figure this out.
+
+{- Ex. 2: Folding with trees -}
+data Tree a
+  = Leaf
+  | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+insert :: a -> Tree a -> Tree a
+insert newObj Leaf = Node 0 Leaf newObj Leaf
+insert newObj (Node n Leaf obj tr) = Node (n + 1) (insert newObj Leaf) obj tr
+insert newObj (Node n tl obj Leaf) = Node n tl obj (insert newObj Leaf)
+insert newObj node@(Node n tl@(Node n' tl' obj' tr') obj tr@(Node n'' tl'' obj'' tr''))
+  | n' > n'' = Node n tl obj (insert newObj tr) -- to the right
+  | otherwise = Node (n + 1) (insert newObj tl) obj tr -- to the left
+
+foldTree :: [a] -> Tree a
+foldTree = foldr insert Leaf
+
+{- Ex. 3: More folds! -}
+{- Ex. 3.1. -}
+xorBin :: Bool -> Bool -> Bool
+xorBin True False = True
+xorBin False True = True
+xorBin _ _ = False
+
+xor :: [Bool] -> Bool
+xor = foldr xorBin False
+
+{- Ex. 3.2 -}
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x acc -> f x : acc) []
+
+{- Ex. 3.3 -}
+-- myFoldl :: (a -> b -> a) -> a -> [b] -> a
+-- myFoldl f base xs = ...
+
+-- TODO: ex. 3.3!
+
+{- Ex. 4: Finding primes -}
+--- Given an integer n, generate odd primes up to 2n + 2
+-- sieveSundaram :: Integer -> [Integer]
+-- sieveSundaram = ...
+
+-- TODO: the Sieve of Sundaram
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x, y) | x <- xs, y <- ys]
--- brent/homework04/test.hs
+++ brent/homework04/test.hs
@@ -1,4 +1,5 @@
 module TestWholemealN where
+
 {-
 1. Install the test libraries:
 cabal install HUnit
@@ -17,15 +18,42 @@ main = defaultMain tests
 tests =
   testGroup
     "Homework 04 tests"
-    [ testCase "fun1: Base case" $
+    [ testCase "Ex. 1, fun1: Base case" $
         fun1' [] @?= fun1 [],
-      testCase "fun1: Only odd numbers" $
+      testCase "Ex. 1, fun1: Only odd numbers" $
         fun1' [7, 13, 3, 9] @?= fun1 [7, 13, 3, 9],
-      testCase "fun1: Even and odd numbers" $
+      testCase "Ex. 1, fun1: Even and odd numbers" $
         fun1' [9, 4, 123, 55, 6] @?= fun1 [9, 4, 123, 55, 6],
-      testCase "fun1: Even and odd numbers, including 2" $
+      testCase "Ex. 1, fun1: Even and odd numbers, including 2" $
         fun1' [2, 1024, 88, 97, 137, -513]
           @?= fun1 [2, 1024, 88, 97, 137, -513],
-      testCase "fun2: Check all inputs in range [1 .. 50]" $
-        map fun2' [1 .. 50] @?= map fun2 [1 .. 50]
+      testCase "Ex. 1, fun2: Check all inputs in range [1 .. 50]" $
+        map fun2' [1 .. 50] @?= map fun2 [1 .. 50],
+      testCase "Ex. 2: foldTree with strings" $
+        foldTree "ABCDEFGHIJ"
+          @?= Node
+            3
+            ( Node
+                2
+                (Node 0 Leaf 'F' Leaf)
+                'I'
+                (Node 1 (Node 0 Leaf 'B' Leaf) 'C' Leaf)
+            )
+            'J'
+            ( Node
+                2
+                (Node 1 (Node 0 Leaf 'A' Leaf) 'G' Leaf)
+                'H'
+                (Node 1 (Node 0 Leaf 'D' Leaf) 'E' Leaf)
+            ),
+      testCase "Ex. 3.1, xor: An odd number of True values" $
+        xor [False, True, False] @?= True,
+      testCase "Ex. 3.1, xor: An even number of True values" $
+        xor [False, True, False, False, True] @?= False,
+      testCase "Ex. 3.2, map': +2" $
+        map' (+ 2) [3, 17, 42] @?= map (+ 2) [3, 17, 42],
+      testCase "Ex. 3.2, map': || False" $
+        map' (|| False) [True, False, False]
+          @?= map (|| False) [True, False, False]
+          -- testCase "Ex. 3.3, myFoldl" $ myFoldl ... @?= foldl ...,
     ]
