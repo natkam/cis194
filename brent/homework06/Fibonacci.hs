04ea683261699f1736d99aaba25ea00979bb2edd
natkam <natka_m@o2.pl>
2025-01-22 21:09:18 +0100

    CIS194, hw6: ex. 3 & 4 - streams and utils

--- brent/homework06/Fibonacci.hs
+++ brent/homework06/Fibonacci.hs
@@ -8,3 +8,20 @@ fibs1 = [fib n | n <- [0 ..]]
 
 fibs2 :: [Integer]
 fibs2 = [0, 1] ++ [fibs2 !! (n - 1) + fibs2 !! (n - 2) | n <- [2 ..]]
+
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+instance Show a => Show (Stream a) where
+  show xs = show $ take 20 (streamToList xs)
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons x (streamFromSeed f (f x))
