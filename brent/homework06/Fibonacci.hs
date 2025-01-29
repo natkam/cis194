7b6bd37d1e7b082fe5c923569e1f6bd20f29af6d
natkam <natka_m@o2.pl>
2025-01-29 23:22:26 +0100

    CIS194, HW6: preparations for ex.5 & 6 (neither finished)

--- brent/homework06/Fibonacci.hs
+++ brent/homework06/Fibonacci.hs
@@ -36,3 +36,28 @@ streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
 streamFromSeed f x = Cons x (streamFromSeed f (f x))
+
+-- ex. 5
+
+nats :: Stream Integer
+nats = streamFromSeed (+ 1) 0
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x1 xs1) xs2 = Cons x1 (interleaveStreams xs2 xs1)
+
+--TODO! (ex. 5.)
+-- ruler :: Stream Integer
+-- ruler = streamMap () (nats)
+
+-- ex. 6
+x :: Stream Integer
+x = Cons 0 (Cons 1 (streamRepeat 0))
+
+instance Num (Stream Integer) where
+  fromInteger n = Cons n (streamRepeat 0)
+  negate = streamMap (* (-1))
+  (+) (Cons a0 as') (Cons b0 bs') = Cons (a0 + b0) (as' + bs')
+  (*) (Cons a0 as') bs@(Cons b0 bs') = Cons (a0 * b0) (streamMap (* a0) bs' + as' * bs)
+
+instance Fractional (Stream Integer) where
+  (/) as@(Cons a0 as') bs@(Cons b0 bs') = Cons (a0 `div` b0) (streamMap (div b0) (as' - as / bs * bs'))
