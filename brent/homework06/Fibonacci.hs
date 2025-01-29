b32e67ba47a852121248cdb7fb25522d78ad136c
natkam <natka_m@o2.pl>
2025-01-29 23:21:40 +0100

    CIS194, HW6: ex. 1-4 - small tweaks

--- brent/homework06/Fibonacci.hs
+++ brent/homework06/Fibonacci.hs
@@ -1,13 +1,22 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods #-}
+
+-- ex. 1
+
 fib :: Integer -> Integer
 fib n
   | n < 2 = n
-  | otherwise = fib (n -1) + fib (n - 2)
+  | otherwise = fib (n - 1) + fib (n - 2)
 
 fibs1 :: [Integer]
 fibs1 = [fib n | n <- [0 ..]]
 
+-- ex. 2
+
 fibs2 :: [Integer]
 fibs2 = [0, 1] ++ [fibs2 !! (n - 1) + fibs2 !! (n - 2) | n <- [2 ..]]
+
+-- ex. 3
 
 data Stream a = Cons a (Stream a)
 
@@ -15,7 +24,9 @@ streamToList :: Stream a -> [a]
 streamToList (Cons x xs) = x : streamToList xs
 
 instance Show a => Show (Stream a) where
-  show xs = show $ take 20 (streamToList xs)
+  show = show . take 20 . streamToList
+
+-- ex. 4
 
 streamRepeat :: a -> Stream a
 streamRepeat x = Cons x (streamRepeat x)
