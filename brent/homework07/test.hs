7802e04ccc18c73a6b11a30fc80f2adf49b0d2f3
natkam <natka_m@o2.pl>
2025-02-03 00:06:42 +0100

    CIS194, hw7: ex. 2 - dropJ, takeJ, plus the tests

--- brent/homework07/test.hs
+++ brent/homework07/test.hs
@@ -7,7 +7,7 @@ import Test.Tasty.HUnit (testCase, (@?=))
 
 main = defaultMain tests
 
--- Helper functions for testing indexJ (ex. 2)
+-- Helper functions (ex. 2)
 (!!?) :: [a] -> Int -> Maybe a
 [] !!? _ = Nothing
 _ !!? i | i < 0 = Nothing
@@ -19,17 +19,27 @@ jlToList Empty = []
 jlToList (Single _ a) = [a]
 jlToList (Append _ jl1 jl2) = jlToList jl1 ++ jlToList jl2
 
-casesIndexJ :: [Bool]
-casesIndexJ = [indexJ i t == (jlToList t !!? i) | i <- [-5 .. 5]]
+jl :: JoinList Size Char
+jl = (l1 +++ (l2 +++ l3)) +++ l4
   where
     l1 = Single (Size 1) 'y'
     l2 = Single (Size 1) 'e'
     l3 = Single (Size 1) 'a'
     l4 = Single (Size 1) 'h'
-    t = (l1 +++ (l2 +++ l3)) +++ l4
+
+casesIndexJ :: [Bool]
+casesIndexJ = [indexJ i jl == (jlToList jl !!? i) | i <- [-5 .. 5]]
+
+casesDropJ :: [Bool]
+casesDropJ = [jlToList (dropJ n jl) == drop n (jlToList jl) | n <- [-5 .. 5]]
+
+casesTakeJ :: [Bool]
+casesTakeJ = [jlToList (takeJ n jl) == take n (jlToList jl) | n <- [-5 .. 5]]
 
 tests =
   testGroup
     "Homework 7 tests"
-    [ testCase "Ex. 2, indexJ" $ and casesIndexJ @?= True
+    [ testCase "Ex. 2, indexJ" $ and casesIndexJ @?= True,
+      testCase "Ex. 2, dropJ" $ and casesDropJ @?= True,
+      testCase "Ex. 2, takeJ" $ and casesTakeJ @?= True
     ]
