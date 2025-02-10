6a88ea48e19c58a2fcbe19387583323e7c86b2a0
natkam <natka_m@o2.pl>
2025-02-10 20:27:23 +0100

    CIS194, hw8: ex. 5 - print out the list of employees in the guest list
    But getting the optimal guest list still doesn't work!

--- brent/homework08/Party.hs
+++ brent/homework08/Party.hs
@@ -1,5 +1,6 @@
 module Party where
 
+import Data.List (sort)
 import Data.Tree (Tree (Node))
 import Employee (Employee (Emp, empFun, empName), GuestList (GL))
 
@@ -10,8 +11,6 @@ glCons e (GL es totalFun) = GL (e : es) (totalFun + empFun e)
 instance Monoid GuestList where
   mempty = GL [] 0
 
--- GL es1 fun1 `mappend` GL es2 fun2 = GL (es1 ++ es2) (fun1 + fun2)
-
 instance Semigroup GuestList where
   GL es1 fun1 <> GL es2 fun2 = GL (es1 ++ es2) (fun1 + fun2)
 
@@ -21,7 +20,7 @@ moreFun = max
 -- ex. 2
 -- TODO!
 treeFold :: (a -> [b] -> b) -> b -> Tree a -> b
-treeFold f z t@(Node _ []) = z
+treeFold f z (Node _ []) = z
 treeFold f z (Node root sts) = f root (map (treeFold f z) sts)
 
 -- ex. 3
@@ -30,6 +29,7 @@ nextLevel boss [] = (GL [boss] (empFun boss), GL [] 0)
 nextLevel boss sublists =
   ( foldr ((<>) . snd) (GL [boss] (empFun boss)) sublists,
     foldr1 (<>) (map fst sublists)
+    -- take max (map fst sublists) (map snd sublists)
   )
 
 -- ex. 4
@@ -40,6 +40,15 @@ maxFun = uncurry moreFun . treeFold nextLevel (GL [] 0, GL [] 0)
 readTree :: String -> Tree Employee
 readTree = read
 
+sortNames :: [Employee] -> [String]
+sortNames emps = sort $ map ((++ "\n") . empName) emps
+
+printGL :: GuestList -> String
+printGL (GL guests totalFun) =
+  "Total fun: " ++ show totalFun ++ "\n"
+    ++ concat (sortNames guests)
+
 main :: IO ()
 main = do
-  tree <- (readTree . readFile) "company.txt"
+  tree <- readFile "company.txt"
+  print $ (printGL . maxFun . readTree) tree
