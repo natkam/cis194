527e37ca1e810226ac8b0f9c67c06bfd10289b5e
natkam <3858688+natkam@users.noreply.github.com>
2025-03-06 02:12:43 +0100

    CIS194, HW1: Write passing tasty tests

--- cis194/homework1/test.hs
+++ cis194/homework1/test.hs
@@ -5,41 +5,23 @@ module TestCardNumbers where
 cabal install HUnit
 cabal install tasty
 cabal install tasty-hunit
+2. Run the tests:
+runghc test.hs
 -}
 
---import Test.Tasty
---import Test.Tasty.HUnit
-import Test.HUnit
 import CardNumbers
-  
-{-
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main = defaultMain tests
 
-tests = testGroup "Exercise 1 tests" [
-  testCase "Convert integer to a list of digits" $
-    toDigits 1234 @?= [1, 2, 3, 4],
-  testCase "Convert 0 to an empty list" $
-    toDigits 0 @?= [],
-  testCase "Convert negative integer to an empty list",
-    toDigits (-17) @?= []]
--}
-
-testToDigits :: Test
-testToDigits = 
-  TestCase $ assertEqual "Convert integer to a list of digits"
-                         (toDigits 1234) [1, 2, 3, 4]
-
-testToDigitsZero :: Test
-testToDigitsZero = 
-  TestCase $ assertEqual "Convert 0 to an empty list"
-                         (toDigits 0) []
-
-testToDigitsNegative :: Test
-testToDigitsNegative = 
-  TestCase $ assertEqual "Convert negative integer to an empty list"
-                         (toDigits (-17)) []
-
-main :: IO Counts
-main = runTestTT $ TestList [testToDigits, testToDigitsZero, testToDigitsNegative]
-
-
+tests =
+  testGroup
+    "Exercise 1 tests"
+    [ testCase "Convert integer to a list of digits" $
+        toDigits 1234 @?= [1, 2, 3, 4],
+      testCase "Convert 0 to an empty list" $
+        toDigits 0 @?= [],
+      testCase "Convert negative integer to an empty list" $
+        toDigits (-17) @?= []
+    ]
